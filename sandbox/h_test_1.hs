ee40fb17610045dc37db0d119884d005730f375f
Georgiy Lukjanov <georgiylukjanov@gmail.com>
2014-12-08 17:17:57 +0300

    Первые шаги

---
+++ sandbox/h_test_1.hs
@@ -0,0 +1,26 @@
+import FRP.Helm
+import qualified FRP.Helm.Graphics as Graphics
+import qualified FRP.Helm.Keyboard as Keyboard
+import qualified FRP.Helm.Window as Window
+
+spaceShipImg :: Element
+spaceShipImg = Graphics.fittedImage 100 100 "img/space_impact_ship.png"
+
+redInvaderImg :: Element
+redInvaderImg = Graphics.fittedImage 100 100 "img/red_invader.png"
+
+data State = State { mx :: Double, my :: Double }
+
+step :: (Int, Int) -> State -> State
+step (dx, dy) state = state { mx = (10 * (realToFrac dx)) + mx state,
+                              my = (10 * (realToFrac dy)) + my state }
+
+render :: (Int, Int) -> State -> Element
+render (w, h) (State { mx = mx, my = my }) =
+  centeredCollage w h [move (mx, my) $ toForm $ spaceShipImg]
+
+main :: IO ()
+main = run defaultConfig $ render <~ Window.dimensions ~~ stepper
+  where
+    state = State { mx = 0, my = 0 }
+    stepper = foldp step state Keyboard.arrows
