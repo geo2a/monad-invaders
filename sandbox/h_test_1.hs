02320c5a9e69ad988a01e39c5394a338e41b5b56
Georgiy Lukjanov <georgiylukjanov@gmail.com>
2014-12-08 18:37:37 +0300

    Добавлен Signal для корабля. Следующий шаг: интегрировать его с main

--- sandbox/h_test_1.hs
+++ sandbox/h_test_1.hs
@@ -1,7 +1,27 @@
 import FRP.Helm
 import qualified FRP.Helm.Graphics as Graphics
 import qualified FRP.Helm.Keyboard as Keyboard
-import qualified FRP.Helm.Window as Window
+import qualified FRP.Helm.Window   as Window
+
+--------------------------------------------------------
+-------------Types for game enteties states-------------
+--------------------------------------------------------
+
+data State = State { x :: Double, y :: Double }
+
+data ShipState = ShipState {shipX :: Int, shipY :: Int} 
+
+--data GameState = 
+
+--------------------------------------------------------
+--------------Initialisation of resources---------------
+--------------------------------------------------------
+
+background :: Element
+background = Graphics.fittedImage 640 480 "img/paper.png"
+
+engineConfig :: EngineConfig
+engineConfig = EngineConfig (640,480) False False "Monad Invaders v0.0.1"
 
 spaceShipImg :: Element
 spaceShipImg = Graphics.fittedImage 100 100 "img/space_impact_ship.png"
@@ -9,18 +29,29 @@ spaceShipImg = Graphics.fittedImage 100 100 "img/space_impact_ship.png"
 redInvaderImg :: Element
 redInvaderImg = Graphics.fittedImage 100 100 "img/red_invader.png"
 
-data State = State { mx :: Double, my :: Double }
+--------------------------------------------------------
+-----------------------Game logic-----------------------
+--------------------------------------------------------
+shipSignal :: Signal ShipState
+shipSignal = signal
+  where initialState = ShipState {shipX = 300, shipY = 400} --TODO: Избавиться от хардкода
+        signal = foldp newState initialState Keyboard.arrows
+        newState :: (Int,Int) -> ShipState -> ShipState
+        newState (dx,dy) state = state {shipX = shipX', shipY = shipY'}
+          where shipX' = shipX state + dx
+                shipY' = shipY state
 
 step :: (Int, Int) -> State -> State
-step (dx, dy) state = state { mx = (10 * (realToFrac dx)) + mx state,
-                              my = (10 * (realToFrac dy)) + my state }
+step (dx, dy) state = state { x = (10 * (realToFrac dx)) + x state,
+                              y = (10 * (realToFrac dy)) + y state }
 
 render :: (Int, Int) -> State -> Element
-render (w, h) (State { mx = mx, my = my }) =
-  centeredCollage w h [move (mx, my) $ toForm $ spaceShipImg]
+render (w, h) (State { x = x, y = y }) =
+  collage w h [toForm background, move (x, y) $ toForm $ spaceShipImg]
 
 main :: IO ()
-main = run defaultConfig $ render <~ Window.dimensions ~~ stepper
+main = run engineConfig $ render <~ Window.dimensions ~~ stepper
   where
-    state = State { mx = 0, my = 0 }
-    stepper = foldp step state Keyboard.arrows
+    initialState = State { x = 300, y = 400} --TODO: Избавиться от хардкода
+    stepper = foldp step initialState Keyboard.arrows
+
